e2980f5ac72cf9883e3bc70f4541b3d81593e531
brunobrolesi <brunohsbrolesi@gmail.com>
2021-11-10 16:27:12 -0300

    test: ensure SignUpController calls AddParkingLot with correct values

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,3 +1,5 @@
+import { ParkingLotModel } from '../../../domain/models/parking-lot-model'
+import { AddParkingLot, AddParkingLotModel } from '../../../domain/usecases/add-parking-lot'
 import { BodyRequestValidator } from '../../protocols/body-request-validator-protocol'
 import { HttpRequest } from '../../protocols/http-protocol'
 import { SignUpController } from './signup-controller'
@@ -12,18 +14,40 @@ const makeBodyRequestValidator = (): BodyRequestValidator => {
   return new SignUpBodyRequestValidatorStub()
 }
 
+const makeAddParkingLot = (): AddParkingLot => {
+  class AddParkingLotStub implements AddParkingLot {
+    async add (parkingLotData: AddParkingLotModel): Promise<ParkingLotModel> {
+      const fakeParkingLot = {
+        id: 'any_id',
+        name: 'any_name',
+        cnpj: 'any_cnpj',
+        address: 'any_address',
+        phone: 'any_phone',
+        carCapacity: 20,
+        motorcycleCapacity: 20
+      }
+      return await new Promise(resolve => resolve(fakeParkingLot))
+    }
+  }
+
+  return new AddParkingLotStub()
+}
+
 interface SutTypes {
   sut: SignUpController
-  bodyValidator: BodyRequestValidator
+  bodyValidatorStub: BodyRequestValidator
+  addParkingLotStub: AddParkingLot
 }
 
 const makeSut = (): SutTypes => {
-  const bodyValidator = makeBodyRequestValidator()
-  const sut = new SignUpController(bodyValidator)
+  const bodyValidatorStub = makeBodyRequestValidator()
+  const addParkingLotStub = makeAddParkingLot()
+  const sut = new SignUpController(bodyValidatorStub, addParkingLotStub)
 
   return {
     sut,
-    bodyValidator
+    bodyValidatorStub,
+    addParkingLotStub
   }
 }
 
@@ -40,19 +64,27 @@ const makeHttpRequest = (): HttpRequest => ({
 
 describe('SignUp Controller', () => {
   it('Should call BodyRequestValidator with correct values', async () => {
-    const { sut, bodyValidator } = makeSut()
-    const validatorSpy = jest.spyOn(bodyValidator, 'validate')
+    const { sut, bodyValidatorStub } = makeSut()
+    const validatorSpy = jest.spyOn(bodyValidatorStub, 'validate')
     const httpRequest = makeHttpRequest()
     await sut.handle(httpRequest)
     expect(validatorSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 
   it('Should returns 400 and error message if BodyRequestValidator returns an error', async () => {
-    const { sut, bodyValidator } = makeSut()
-    jest.spyOn(bodyValidator, 'validate').mockReturnValueOnce(new Error('missing field'))
+    const { sut, bodyValidatorStub } = makeSut()
+    jest.spyOn(bodyValidatorStub, 'validate').mockReturnValueOnce(new Error('missing field'))
     const httpRequest = makeHttpRequest()
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body.error).toBe('missing field')
+  })
+
+  it('Should call AddParkingLot with correct values', async () => {
+    const { sut, addParkingLotStub } = makeSut()
+    const addSpy = jest.spyOn(addParkingLotStub, 'add')
+    const httpRequest = makeHttpRequest()
+    await sut.handle(httpRequest)
+    expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 })
