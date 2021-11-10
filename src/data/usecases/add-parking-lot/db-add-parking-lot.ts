14e0e25d56b20b5f321e1ee5b985a5731ae50430
brunobrolesi <brunohsbrolesi@gmail.com>
2021-11-10 19:56:15 -0300

    feat: ensure DbAddParkingLot calls Hasher with correct password

--- src/data/usecases/add-parking-lot/db-add-parking-lot.ts
+++ src/data/usecases/add-parking-lot/db-add-parking-lot.ts
@@ -1,11 +1,13 @@
 import { ParkingLotModel } from '../../../domain/models/parking-lot-model'
 import { AddParkingLot, AddParkingLotModel } from '../../../domain/usecases/add-parking-lot'
+import { Hasher } from '../../protocols/cryptography/hasher'
 import { AddParkingLotRepository } from '../../protocols/db/add-parking-lot-repository'
 import { LoadParkingLotByEmailRepository } from '../../protocols/db/load-parking-lot-by-email-repository'
 
 export class DbAddParkingLot implements AddParkingLot {
   constructor (
     private readonly loadParkingLotByEmailRepository: LoadParkingLotByEmailRepository,
+    private readonly hasher: Hasher,
     private readonly addParkingLotRepository: AddParkingLotRepository
   ) {}
 
@@ -14,6 +16,8 @@ export class DbAddParkingLot implements AddParkingLot {
 
     if (hasAccount) return null
 
-    await this.addParkingLotRepository.add(parkingLotData)
+    const hashedPassword = this.hasher.hash(parkingLotData.password)
+
+    await this.addParkingLotRepository.add({ ...parkingLotData, password: hashedPassword })
   }
 }
