f6557db78996301116434469a5921796251d5524
brunobrolesi <brunohsbrolesi@gmail.com>
2021-11-10 15:50:41 -0300

    refactor: move sut, httpRequest, bodyValidator creation to methods

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,26 +1,48 @@
 import { BodyRequestValidator } from '../../protocols/body-request-validator'
+import { httpRequest } from '../../protocols/http-protocol'
 import { SignUpController } from './signup-controller'
+
+const makeBodyRequestValidator = (): BodyRequestValidator => {
+  class SignUpBodyRequestValidatorStub implements BodyRequestValidator {
+    validate (body: object): Error|null {
+      return null
+    }
+  }
+
+  return new SignUpBodyRequestValidatorStub()
+}
+
+interface SutTypes {
+  sut: SignUpController
+  bodyValidator: BodyRequestValidator
+}
+
+const makeSut = (): SutTypes => {
+  const bodyValidator = makeBodyRequestValidator()
+  const sut = new SignUpController(bodyValidator)
+
+  return {
+    sut,
+    bodyValidator
+  }
+}
+
+const makeHttpRequest = (): httpRequest => ({
+  body: {
+    name: 'any_name',
+    cnpj: 'any_cnpj',
+    address: 'any_address',
+    phone: 'any_phone',
+    carCapacity: 20,
+    motorcycleCapacity: 20
+  }
+})
 
 describe('SignUp Controller', () => {
   it('Should call BodyRequestValidator with correct values', async () => {
-    class SignUpBodyRequestValidatorStub implements BodyRequestValidator {
-      validate (body: object): Error|null {
-        return null
-      }
-    }
-    const bodyValidator = new SignUpBodyRequestValidatorStub()
-    const sut = new SignUpController(bodyValidator)
+    const { sut, bodyValidator } = makeSut()
     const validatorSpy = jest.spyOn(bodyValidator, 'validate')
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        cnpj: 'any_cnpj',
-        address: 'any_address',
-        phone: 'any_phone',
-        carCapacity: 20,
-        motorcycleCapacity: 20
-      }
-    }
+    const httpRequest = makeHttpRequest()
     await sut.handle(httpRequest)
     expect(validatorSpy).toHaveBeenCalledWith(httpRequest.body)
   })
