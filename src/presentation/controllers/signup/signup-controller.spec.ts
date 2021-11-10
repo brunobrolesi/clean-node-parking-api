b6af776820d7d84d31d9ade9a5396ab3171b9b7c
brunobrolesi <brunohsbrolesi@gmail.com>
2021-11-10 16:59:56 -0300

    test: ensure SignUpController returns 201 and parking lot in AddParkingLot success

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -14,21 +14,24 @@ const makeBodyRequestValidator = (): BodyRequestValidator => {
   return new SignUpBodyRequestValidatorStub()
 }
 
+const makeFakeParkingLot = (): ParkingLotModel => (
+  {
+    id: 'any_id',
+    email: 'any_email',
+    password: 'any_password',
+    name: 'any_name',
+    cnpj: 'any_cnpj',
+    address: 'any_address',
+    phone: 'any_phone',
+    carCapacity: 20,
+    motorcycleCapacity: 20
+  }
+)
+
 const makeAddParkingLot = (): AddParkingLot => {
   class AddParkingLotStub implements AddParkingLot {
     async add (parkingLotData: AddParkingLotModel): Promise<ParkingLotModel> {
-      const fakeParkingLot = {
-        id: 'any_id',
-        email: 'any_email',
-        password: 'any_password',
-        name: 'any_name',
-        cnpj: 'any_cnpj',
-        address: 'any_address',
-        phone: 'any_phone',
-        carCapacity: 20,
-        motorcycleCapacity: 20
-      }
-      return await new Promise(resolve => resolve(fakeParkingLot))
+      return await new Promise(resolve => resolve(makeFakeParkingLot()))
     }
   }
 
@@ -79,8 +82,7 @@ describe('SignUp Controller', () => {
   it('Should returns 400 and error message if BodyRequestValidator returns an error', async () => {
     const { sut, bodyValidatorStub } = makeSut()
     jest.spyOn(bodyValidatorStub, 'validate').mockReturnValueOnce(new Error('missing field'))
-    const httpRequest = makeHttpRequest()
-    const httpResponse = await sut.handle(httpRequest)
+    const httpResponse = await sut.handle(makeHttpRequest())
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body.error).toBe('missing field')
   })
@@ -96,9 +98,15 @@ describe('SignUp Controller', () => {
   it('Should returns 500 and internal server error message if AddParkingLot throws', async () => {
     const { sut, addParkingLotStub } = makeSut()
     jest.spyOn(addParkingLotStub, 'add').mockRejectedValueOnce(new Error())
-    const httpRequest = makeHttpRequest()
-    const httpResponse = await sut.handle(httpRequest)
+    const httpResponse = await sut.handle(makeHttpRequest())
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body.error).toBe('internal server error')
+  })
+
+  it('Should returns 201 and parking lot in AddParkingLot success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeHttpRequest())
+    expect(httpResponse.statusCode).toBe(201)
+    expect(httpResponse.body.data).toEqual(makeFakeParkingLot())
   })
 })
