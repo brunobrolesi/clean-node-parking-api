9a4f57c4fe601d73cfd91f2c83a266686640abfb
brunobrolesi <brunohsbrolesi@gmail.com>
2021-11-10 19:40:07 -0300

    test: ensure DbAddParkingLot calls AddParkingLotRepository with correct values

--- src/data/usecases/add-parking-lot/db-add-parking-lot.spec.ts
+++ src/data/usecases/add-parking-lot/db-add-parking-lot.spec.ts
@@ -1,29 +1,43 @@
 import { ParkingLotModel } from '../../../domain/models/parking-lot-model'
 import { AddParkingLotModel } from '../../../domain/usecases/add-parking-lot'
-import { DbLoadParkingLotByEmailRepository } from '../../protocols/db-load-parking-lot-by-email'
+import { AddParkingLotRepository } from '../../protocols/db/add-parking-lot-repository'
+import { LoadParkingLotByEmailRepository } from '../../protocols/db/load-parking-lot-by-email-repository'
 import { DbAddParkingLot } from './db-add-parking-lot'
 
-const makeLoadParkingLotByEmailRepository = (): DbLoadParkingLotByEmailRepository => {
-  class DbLoadParkingLotByEmailRepositoryStub implements DbLoadParkingLotByEmailRepository {
+const makeLoadParkingLotByEmailRepository = (): LoadParkingLotByEmailRepository => {
+  class LoadParkingLotByEmailRepositoryStub implements LoadParkingLotByEmailRepository {
     async loadByEmail (email: string): Promise<ParkingLotModel> {
       return await new Promise(resolve => resolve(null))
     }
   }
 
-  return new DbLoadParkingLotByEmailRepositoryStub()
+  return new LoadParkingLotByEmailRepositoryStub()
+}
+
+const makeAddParkingLotRepository = (): AddParkingLotRepository => {
+  class AddParkingLotRepositoryStub implements AddParkingLotRepository {
+    async add (parkingLotData: AddParkingLotModel): Promise<ParkingLotModel> {
+      return await new Promise(resolve => resolve(makeFakeParkingLotModel()))
+    }
+  }
+
+  return new AddParkingLotRepositoryStub()
 }
 
 interface SutTypes {
   sut: DbAddParkingLot
-  loadParkingLotByEmailRepositoryStub: DbLoadParkingLotByEmailRepository
+  loadParkingLotByEmailRepositoryStub: LoadParkingLotByEmailRepository
+  addParkingLotRepositoryStub: AddParkingLotRepository
 }
 
 const makeSut = (): SutTypes => {
   const loadParkingLotByEmailRepositoryStub = makeLoadParkingLotByEmailRepository()
-  const sut = new DbAddParkingLot(loadParkingLotByEmailRepositoryStub)
+  const addParkingLotRepositoryStub = makeAddParkingLotRepository()
+  const sut = new DbAddParkingLot(loadParkingLotByEmailRepositoryStub, addParkingLotRepositoryStub)
   return {
     sut,
-    loadParkingLotByEmailRepositoryStub
+    loadParkingLotByEmailRepositoryStub,
+    addParkingLotRepositoryStub
   }
 }
 
@@ -65,10 +79,17 @@ describe('DbAddParkingLot UseCase', () => {
     expect(parking).toBeNull()
   })
 
-  it('Should throws if LoadParkingLotByEmailRepository throws', async () => {
+  it('Should throw if LoadParkingLotByEmailRepository throws', async () => {
     const { sut, loadParkingLotByEmailRepositoryStub } = makeSut()
     jest.spyOn(loadParkingLotByEmailRepositoryStub, 'loadByEmail').mockRejectedValueOnce(new Error())
     const promise = sut.add(makeFakeParkingLotData())
     await expect(promise).rejects.toThrow()
+  })
+
+  it('Should call AddParkingLotRepository with correct values', async () => {
+    const { sut, addParkingLotRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addParkingLotRepositoryStub, 'add')
+    await sut.add(makeFakeParkingLotData())
+    expect(addSpy).toHaveBeenCalledWith(makeFakeParkingLotData())
   })
 })
