13d71e013d618224c2c83caf845e904da90d6b35
brunobrolesi <brunohsbrolesi@gmail.com>
2021-11-10 19:11:12 -0300

    test: ensure DbAddParkingLot returns null if LoadParkingLotByEmailRepositoryreturns a parking lot

--- src/data/usecases/add-parking-lot/db-add-parking-lot.spec.ts
+++ src/data/usecases/add-parking-lot/db-add-parking-lot.spec.ts
@@ -38,11 +38,30 @@ const makeFakeParkingLotData = (): AddParkingLotModel => ({
   motorcycleCapacity: 20
 })
 
+const makeFakeParkingLotModel = (): ParkingLotModel => ({
+  id: 'any_id',
+  email: 'any_email',
+  password: 'any_password',
+  name: 'any_name',
+  cnpj: 'any_cnpj',
+  address: 'any_address',
+  phone: 'any_phone',
+  carCapacity: 20,
+  motorcycleCapacity: 20
+})
+
 describe('DbAddParkingLot UseCase', () => {
   it('Should call LoadParkingLotByEmailRepository with correct email', async () => {
     const { sut, loadParkingLotByEmailRepositoryStub } = makeSut()
     const loadByEmailSpy = jest.spyOn(loadParkingLotByEmailRepositoryStub, 'loadByEmail')
     await sut.add(makeFakeParkingLotData())
     expect(loadByEmailSpy).toHaveBeenCalledWith(makeFakeParkingLotData().email)
+  })
+
+  it('Should return null if LoadParkingLotByEmailRepository returns a parking lot', async () => {
+    const { sut, loadParkingLotByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadParkingLotByEmailRepositoryStub, 'loadByEmail').mockResolvedValueOnce(makeFakeParkingLotModel())
+    const parking = await sut.add(makeFakeParkingLotData())
+    expect(parking).toBeNull()
   })
 })
