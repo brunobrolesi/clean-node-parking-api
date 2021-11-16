262a4761e3718fed37edf1765a63e1f5d98ffa00
brunobrolesi <brunohsbrolesi@gmail.com>
2021-11-16 18:42:10 -0300

    feat: ensure PostgreSqlParkingLotRepository calls prisma client parking lot creation with correct values

---
+++ src/infra/db/postgre-sql/postgre-sql-parking-lot-repository.ts
@@ -0,0 +1,25 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import { AddParkingLotRepository } from '../../../data/protocols/db/add-parking-lot-repository'
+import { ParkingLotModel } from '../../../domain/models/parking-lot-model'
+import { AddParkingLotModel } from '../../../domain/usecases/add-parking-lot'
+
+export class PostgreSqlParkingLotRepository implements AddParkingLotRepository {
+  constructor (private readonly prisma: PrismaClient) {}
+
+  async add (parkingLotData: AddParkingLotModel): Promise<ParkingLotModel> {
+    const { carCapacity, motorcycleCapacity, ...data } = parkingLotData
+    const parkingLot: Prisma.ParkingLotCreateInput = {
+      ...data,
+      status: {
+        create: {
+          carCapacity,
+          motorcycleCapacity
+        }
+      }
+    }
+    await this.prisma.parkingLot.create({
+      data: parkingLot
+    })
+    return null
+  }
+}
