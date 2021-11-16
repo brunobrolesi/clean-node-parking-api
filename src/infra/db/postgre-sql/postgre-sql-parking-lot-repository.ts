d8520c9d12fd7e3014e47ab65608661ad14c76a8
brunobrolesi <brunohsbrolesi@gmail.com>
2021-11-16 20:40:28 -0300

    feat: add loadByEmail to PostgreSqlParkingLotRepository and ensure it calls findUnique with correct email, throw if findUnique throws, returns a parking lot on success and returns null if not found email

--- src/infra/db/postgre-sql/postgre-sql-parking-lot-repository.ts
+++ src/infra/db/postgre-sql/postgre-sql-parking-lot-repository.ts
@@ -1,9 +1,10 @@
 import { Prisma, PrismaClient } from '@prisma/client'
 import { AddParkingLotRepository } from '../../../data/protocols/db/add-parking-lot-repository'
+import { LoadParkingLotByEmailRepository } from '../../../data/protocols/db/load-parking-lot-by-email-repository'
 import { ParkingLotModel } from '../../../domain/models/parking-lot-model'
 import { AddParkingLotModel } from '../../../domain/usecases/add-parking-lot'
 
-export class PostgreSqlParkingLotRepository implements AddParkingLotRepository {
+export class PostgreSqlParkingLotRepository implements AddParkingLotRepository, LoadParkingLotByEmailRepository {
   constructor (private readonly prisma: PrismaClient) {}
 
   async add (parkingLotData: AddParkingLotModel): Promise<ParkingLotModel> {
@@ -18,5 +19,9 @@ export class PostgreSqlParkingLotRepository implements AddParkingLotRepository {
       }
     }
     return await this.prisma.parkingLot.create({ data: parkingLotInput })
+  }
+
+  async loadByEmail (email: string): Promise<ParkingLotModel> {
+    return await this.prisma.parkingLot.findUnique({ where: { email } })
   }
 }
